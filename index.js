f171870cd227b21cf0b142d1e2ec3b397e7284f6
Pico Embed <51259980+pico-embed@users.noreply.github.com>
2019-06-01 11:05:54 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-document.write(getAllUrlParams())
+document.write(getAllUrlParams().cart)
 
 function getAllUrlParams(url) {
   var queryString = url ? url.split('?')[1] : window.location.search.slice(1);
